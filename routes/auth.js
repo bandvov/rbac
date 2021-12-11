7ce11f3aaa030afc63f0f4c0de073cd820a687eb
bandvov <bandvov@gmail.com>
2021-12-11 14:24:33 +0200

    added templates for login and register pages

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 router.get("/login", async (req, res) => {
-  res.send("login");
+  res.render("login");
 });
 router.get("/register", async (req, res) => {
-  res.send("register");
+  res.render("register");
 });
 router.post("/login", async (req, res) => {
   res.send("login");
 });
 router.post("/register", async (req, res) => {
-  res.send("register");
+  res.render("register");
 });
 router.get("/logout", async (req, res) => {
   res.send("logout");
