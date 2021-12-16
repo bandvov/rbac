2a5bcf56dd1d92fe4d53588140c5584a9974c3d2
bandvov <bandvov@gmail.com>
2021-12-16 20:58:09 +0200

    add validation for reguster

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const UserModel = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
+router.use("/register", [
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Email format is not correct")
+    .normalizeEmail()
+    .toLowerCase(),
+  body("password").trim().isLength(2).withMessage("Password is too short"),
+]);
 router
   .route("/login")
   .get(async (req, res) => {
@@ -17,14 +27,22 @@ router
   })
   .post(async (req, res, next) => {
     const { email } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      errors.array().forEach((error) => {
+        req.flash("error", error.msg);
+      });
+      res.render("register", { messages: req.flash() });
+    }
     try {
       const userExists = await UserModel.findOne({ email });
       if (userExists) {
         res.redirect("/auth/register");
       }
       const user = await UserModel.create(req.body);
-
-      res.send({ email: user.email });
+      if (user) {
+        res.redirect("/auth/login");
+      }
     } catch (err) {
       next(err);
     }
